2d1c7ed85aa730b78b37068967a55282124e4d38
daisymarie <daisy.m.s@hotmail.com>
2014-07-31 14:40:09 +1000

    datetime changed

--- db/migrate/20140731020634_create_flights.rb
+++ db/migrate/20140731020634_create_flights.rb
@@ -2,8 +2,8 @@ class CreateFlights < ActiveRecord::Migration
   def change
     create_table :flights do |t|
       t.text :name
-      t.date :origin_date
-      t.date :destination_date
+      t.datetime :origin_date
+      t.datetime :destination_date
       t.text :origin
       t.text :destination
       t.integer :airplane_id
