aeead1ad359b7a5ca3dae5caf20a9528439de507
Xaun Lopez <xaunlopez@gmail.com>
2014-07-31 19:00:00 +1000

    create airplanes

--- app/assets/javascripts/views/airplaneView.js
+++ app/assets/javascripts/views/airplaneView.js
@@ -3,6 +3,10 @@ var app = app || {};
 app.AirplaneView = Backbone.View.extend({
   el: '#content',
 
+  events: {
+    'click button': 'createAirplane'
+  },
+
   initialize: function () {
 
   },
@@ -10,6 +14,11 @@ app.AirplaneView = Backbone.View.extend({
   render: function () {
     var airplaneView = Handlebars.compile(app.templates.airplaneView);
     this.$el.html( airplaneView );
+  },
+
+  createAirplane: function () {
+    var newPlane = new app.Airplane( {name: $('#name').val(), row: $('#row').val(), column: $('#column').val()} );
+    newPlane.save();
   }
 
 });
--- app/assets/javascripts/views/flightView.js
+++ app/assets/javascripts/views/flightView.js
@@ -4,7 +4,12 @@ app.FlightView = Backbone.View.extend({
   el: '#content',
 
   initialize: function () {
-
+    // Trying to fetch from database when initializing flightView.
+    // console.log('flightView initialized');
+    // app.airplanes = new app.Airplanes();
+    // app.airplanes.fetch().done(function () {
+    //   Backbone.history.start();
+    // });
   },
 
   render: function () {
